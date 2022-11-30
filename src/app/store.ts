052ea33b98a674446f6cd5802f75897ecd301e7c
GB <voducthang16@gmail.com>
2022-11-30 09:41:34 +0700

    Add Redux Persist
    30.11.2022 - 9:15 AM

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,19 +1,37 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import productReducer from '~/features/product/productSlice';
 import orderReducer from '~/features/order/orderSlice';
 import categoryReducer from '~/features/category/categorySlice';
 import tableReducer from '~/features/table/tableSlice';
-export const store = configureStore({
-    reducer: {
-        counter: counterReducer,
-        order: orderReducer,
-        category: categoryReducer,
-        product: productReducer,
-        table: tableReducer,
-    },
+import { persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    order: orderReducer,
+    category: categoryReducer,
+    product: productReducer,
+    table: tableReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+export const persistor = persistStore(store);
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import App from './App';
 import './index.css';
 import GlobalStyles from './components/GlobalStyles';
 import { ChakraProvider } from '@chakra-ui/react';
-
+import { PersistGate } from 'redux-persist/integration/react';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
@@ -14,7 +14,9 @@ root.render(
     <React.StrictMode>
         <ChakraProvider>
             <Provider store={store}>
-                <GlobalStyles Children={App} />
+                <PersistGate loading={null} persistor={persistor}>
+                    <GlobalStyles Children={App} />
+                </PersistGate>
             </Provider>
         </ChakraProvider>
     </React.StrictMode>,
